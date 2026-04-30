5a20a29285d39e23dce6f48c705c3e3d0da9a98a
Task User <user@example.com>
2026-04-30 12:02:16 +0300

    failed

--- test.py
+++ test.py
@@ -3,11 +3,11 @@ import unittest
 # Simple arithmetic methods
 def add(a, b):
     """Add two numbers"""
-    return a + b
+    return a - b
 
 def subtract(a, b):
     """Subtract two numbers"""
-    return a *b
+    return a * b
 
 def multiply(a, b):
     """Multiply two numbers"""
